3f3e8c7d9d9827b032838709a12c7ec8793a2c38
lucaslrp <lucaslrpassos@gmail.com>
2024-05-08 23:17:02 -0300

    adding the guide for the first TCs

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,5 +3,8 @@
 describe('Home Page', () => {
   it('Page should be On', () => {
     cy.visit('/')
+
+    cy.title()
+      .should('eq', 'A place to practice your automation skills!')
   })
 })
---
+++ cypress/e2e/tests.cy.js
@@ -0,0 +1,51 @@
+/// <reference types="cypress"/>
+
+describe('Home Page', () => {
+
+    context('Home Page - Top part', () => {
+
+        it('Validate HOME menu selected', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+        it('Validate Car empty', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+        it('Validate Currency Options', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+        it('Validate all Menu Options redirection', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+        it('Validate 3 slides', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+        it('Validate Red Message under the Slides', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+        it('Validate Social Media Icons', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+        it('Validate Search Keywords', () => {
+            cy.visit('/')
+            cy.get('').should('be.visible')
+
+        })
+
+    })
+
+//Validate background color
+})   
