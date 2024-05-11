9a31894313a54a44e727e98edb608e8b2551b5db
lucaslrp <lucaslrpassos@gmail.com>
2024-05-10 23:05:30 -0300

    new tcs

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -13,6 +13,7 @@ describe('Home Page', () => {
                 .and('eq', 'rgb(0, 161, 203)')
 
         })
+
         it('Validate Cart empty', () => {
             cy.visit('/')
 
@@ -21,6 +22,7 @@ describe('Home Page', () => {
                 .should('have.text', '$0.00')
 
         })
+
         it('Validate Currency Options', () => {
             cy.visit('/')
 
@@ -42,7 +44,8 @@ describe('Home Page', () => {
                 })
 
         })
-        it.only('Validate all Menu Options redirection', () => {
+        
+        it('Validate all Menu Options', () => {
             cy.visit('/')
             /*Each menu:
             cy.get('.nav-pills  > :nth-child(2)') ----> Apparel and accessories / url final: 68
@@ -65,38 +68,88 @@ describe('Home Page', () => {
                 })
 
             //Validate if when clicked, It'll redirect to the correct page
-            const urlNumbers = ['68', '36', '43', '49', '58', '52', '65']
-            const urlWithoutNumber = 'https://automationteststore.com/index.php?rt=product/category&path='        
-
-            /*cy.get('.nav-pills > li')
+            /*const menuPageTitles = ['Apparel & accessories', 'Makeup', 'Skincare', 'Fragrance', 'Men', 'Hair Care', 'Books']
+            cy.get('.nav-pills > li')
                 .each(($li, index) => {
                     if (index >= 1) {
-                        const urlNumber = urlNumbers[index -1]
-                        cy.wrap($li).should('have.attr', 'href').and('eq', `${urlWithoutNumber}${urlNumber}`)
+                        const menuPageTitle = menuPageTitles[index - 1]
+                        cy.wrap($li).click('center')
+                        cy.wait(5000)
+                        cy.title()
+                            .should('eq', menuPageTitle)
                     }
                 })*/
 
         })
+
         it('Validate 3 slides', () => {
             cy.visit('/')
-            cy.get('').should('be.visible')
+
+            //validating qtt of slides
+            cy.get('.oneByOneSlide > #banner_slides > .oneByOne_item')
+            .should('have.length', 3)
+            
+            //validate arrow buttons
+            //Previous Arrow click
+            cy.get('.oneByOneSlide')
+            .trigger('mouseover')
+            cy.get('.prevArrow').should('be.visible')
+            .click()
+            .should('have.css', 'display')
+            .end('eq', 'block')
+            //Next Arrow click
+            cy.get('.nextArrow')
+            .should('be.visible')
+            .click()
+            .should('have.css', 'display')
+            .end('eq', 'block')
 
         })
+
         it('Validate Red Message under the Slides', () => {
             cy.visit('/')
-            cy.get('').should('be.visible')
+
+            //Color = #D13614 / in RGB = (209, 54, 20)
+            cy.get('.welcome_msg')
+            .should('be.visible')
+            .and('contain.text', 'Welcome to the Automation Test Store! ')
+
+            cy.get('.welcome_msg > h4')
+            .and('have.css', 'color')
+            .and('eq', 'rgb(209, 54, 20)')
 
         })
-        it('Validate Social Media Icons', () => {
+
+        it('Validate Promo Block', () => {
+            cy.visit('/')
+
+            const promoSectionTitles = ['Fast shipping', 'Easy Payments', 'Shipping Options', 'Large Variety']
+            //Count Icons
+            cy.get('.col-md-12 > .row > .row > .col-md-3')
+            .should('be.visible').should('have.length', 4)
+            //Validate each title
+            cy.get('.col-md-12 > .row > .row > .col-md-3 > .promo_text > h2').each(($text, index)=>{
+                const promoSectionTitle = promoSectionTitles[index]
+                cy.wrap($text).should('contains.text', promoSectionTitle)
+            })
+            
+            /*each(($class, index)=>{
+                const promoSectionTitle = promoSectionTitles[index]
+                cy.wrap($class).should('have.class', 'promo_text').and('have.text', promoSectionTitle)
+            })*/
+        })
+
+       /* it('Validate Social Media Icons', () => {
             cy.visit('/')
             cy.get('').should('be.visible')
 
         })
-        it('Validate Search Keywords', () => {
+
+      //  it('Validate Search Keywords', () => {
             cy.visit('/')
             cy.get('').should('be.visible')
 
-        })
+        })*/
 
     })
 
